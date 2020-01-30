8596d44ec3ebd55301402ba728494020ebfb96a0
Paulo Henrique Ortolan <paulo.ortolan@criticaltechworks.com>
2020-01-30 18:36:29 +0000

    More code

---
+++ src/main/java/pho/blog/bot/core/handlers/HandlerResult.java
@@ -0,0 +1,36 @@
+package pho.blog.bot.core.handlers;
+
+import java.io.File;
+
+public final class HandlerResult {
+
+    public static final String NO_ID = "NO_ID";
+
+    private final MessageType messageType;
+    private final String fileId;
+    private final File file;
+
+    public HandlerResult(MessageType messageType, String fileId, File file) {
+        this.messageType = messageType;
+        this.fileId = fileId;
+        this.file = file;
+    }
+
+    public HandlerResult(MessageType messageType, File file) {
+        this.messageType = messageType;
+        this.fileId = NO_ID;
+        this.file = file;
+    }
+
+    public MessageType getMessageType() {
+        return messageType;
+    }
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public File getFile() {
+        return file;
+    }
+}
---
+++ src/main/java/pho/blog/bot/core/handlers/MessageType.java
@@ -0,0 +1,7 @@
+package pho.blog.bot.core.handlers;
+
+public enum MessageType {
+
+    PHOTO, VIDEO, AUDIO, CONTACT, LOCATION, DOCUMENT
+
+}
